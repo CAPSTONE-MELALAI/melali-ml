d7195e3d04ccc5deb8f1814dbf46636a12858f69
337ubaid <devvevan@gmail.com>
2024-06-14 12:49:55 +0700

    update test_fastapi.py

--- deploy/test_fastapi.py
+++ deploy/test_fastapi.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/random")
+async def random_number(): 
+    return {"random_number": random.randint(0, 100)}
