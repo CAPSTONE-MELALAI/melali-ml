e9f897f4e1c4b33d5bf5fef106a16dac8c2ee99a
337ubaid <devvevan@gmail.com>
2024-06-14 16:56:23 +0700

    add POST method

--- deploy/test_fastapi.py
+++ deploy/test_fastapi.py
@@ -10,3 +10,10 @@ async def root():
 @app.get("/random")
 async def random_number(): 
     return {"random_number": random.randint(0, 100)}
+
+@app.post("/login")
+async def login(data: dict):
+    username = data.get("nama")
+    password = data.get("password")
+    # Perform login logic here
+    return {"username":username, "password":password}
